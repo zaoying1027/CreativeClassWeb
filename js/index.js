30fc78d2ca3eb249e185b6b4bd7e4aa610fcac56
zaoying1027 <1747266224@qq.com>
2017-06-15 18:18:46 +0800

    头部的修改

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
 // JavaScript Document
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 800) {
+        $('#menu').fadeIn();
+    }
+    else {
+        $('#menu').fadeOut();
+    }
+
+});
 $(document).ready(function(){
 	$('.carousel').carousel({
 		interval: 2000
